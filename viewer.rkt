67adb202cdd8ab1fde257f3c82ba775d23ce140c
Benjamin Seppke <benjamin.seppke@gmail.com>
2014-05-27 09:44:27 +0200

    major cleanup of the new viewer canvas + mouse and full keypad navigation

--- viewer.rkt
+++ viewer.rkt
@@ -7,50 +7,110 @@
 ;Dependencies: 
 (require racket/gui)
 (require racket/draw)
+(define (rex r)
+  (inexact->exact (round r)))
 
 ;;#############################################################################
 ;;####         GUI for showing images. Clicking inside the image            ###
 ;;####         shows the current pixel values!                              ###
 ;;#############################################################################
 (define (image-value->string image x y)
-  (case (image-numbands image)
-    ;;for gray-images
-    ((1) (real->decimal-string (image-ref image x y 0) 4))
-    ;;for rgb-images
-    ((3)(string-append "[ R:"
-                       (real->decimal-string (image-ref image x y 0) 4)
+  (let ((nx (rex x))
+        (ny (rex y)))
+    (case (image-numbands image)
+      ;;for gray-images
+      ((1) (real->decimal-string (image-ref image nx ny 0) 4))
+      ;;for rgb-images
+      ((3)(string-append "[ R:"
+                         (real->decimal-string (image-ref image nx ny 0) 4)
                        " G:"
-                       (real->decimal-string (image-ref image x y 1) 4)
+                       (real->decimal-string (image-ref image nx ny 1) 4)
                        " B:"
-                       (real->decimal-string (image-ref image x y 2) 4)
-                       " ]"))))
+                       (real->decimal-string (image-ref image nx ny 2) 4)
+                       " ]")))))
 
 ;; Defines a class on the fly that overrides the callback for
 ;; mouse events in a canvas; you could name it,  if needed
 (define image-canvas%
   (class canvas%
     (init-field image [racket_img #f]) 
+    ;; Paint Event Handler -> simply draws the image to the device context (dc%)
     (define/override (on-paint)
       (begin
           (when (boolean? racket_img)
             (set! racket_img (image->racket-image image)))
           (send (send this get-dc) draw-bitmap racket_img 0 0)))
+    ;; Key (and mouse wheel) Handler:
+    ;  Arrow-Keys: move canvas (if possible)
+    ;  Mouse wheel down or Ctrl+"+": Zoom in (+10%)
+    ;  Mouse wheel up or Ctrl+"-": Zoom out (-10%)
+    ;  Ctrl+"0": Reset Zoom to 100%
+    (define/override (on-char e) 
+      (let*-values (((ctrl)  (send e get-control-down))
+                    ((key)   (send e get-key-code))
+                    ((dc)    (send this get-dc))
+                    ((s_x s_y) (send dc get-scale))
+                    ((off_x off_y) (send this get-view-start))
+                    ((cli_w cli_h) (send this get-client-size))
+                    ((scr_x scr_y) (values (/ off_x (- (* (image-width image)  s_x) cli_w))
+                                           (/ off_y (- (* (image-height image) s_y) cli_h))))
+                    ((w h) (values (image-width image) (image-height image))))
+        (when (or (and ctrl (eq? key #\+))
+                  (eq? key 'wheel-down))  
+          (begin 
+            (send dc set-scale (* s_x 1.1)(* s_y 1.1))
+            (send this init-auto-scrollbars
+                  (rex (* w (* s_x 1.1)))
+                  (rex (* h (* s_y 1.1)))
+                  scr_x
+                  scr_y)
+              (send this refresh)))
+          (when (or (and ctrl (eq? key #\-))
+                    (eq? key 'wheel-up)) 
+            (begin 
+              (send dc set-scale (/ s_x 1.1)(/ s_y 1.1))
+              (send this init-auto-scrollbars
+                    (rex (* w (/ s_x 1.1)))
+                    (rex (* h (/ s_y 1.1)))
+                    scr_x
+                    scr_y)
+              (send this refresh)))
+          (when (and ctrl (eq? key #\0)) 
+            (begin 
+              (send dc set-scale 1.0 1.0)
+              (send this init-auto-scrollbars
+                    w
+                    h
+                    scr_x
+                    scr_y)
+              (send this refresh)))
+          (when (eq? key 'up)
+            (send this scroll #f (max 0.0 (- scr_y (/ 1.0 s_y)))))
+          (when (eq? key 'down)
+            (send this scroll #f (min 1.0 (+ scr_y (/ 1.0 s_y)))))
+          (when (eq? key 'left)
+            (send this scroll (max 0.0 (- scr_x (/ 1.0 s_x))) #f))
+          (when (eq? key 'right)
+            (send this scroll (min 1.0 (+ scr_x (/ 1.0 s_x))) #f))))
+    ;; Mouse move event handler
     (define/override (on-event e) 
         (begin
           (when (send e moving?)
             (let-values (((off_x off_y) (send this get-view-start)))
               (let* ((parent (send this get-parent))
-                     (x (+ (send e get-x) off_x))
-                     (y (+ (send e get-y) off_y))
+                     (dc     (send this get-dc))
                      (width  (image-width image))
                      (height (image-height image)))
-                (if (and (< -1 x width)
-                         (< -1 y height))
+                (let-values (((s_x s_y) (send dc get-scale)))
+                  (let* ((x (/ (+ (send e get-x) off_x) s_x))
+                         (y (/ (+ (send e get-y) off_y) s_y)))
+                  (if (and (<= 0 x (- width  1))
+                           (<= 0 y (- height 1)))
                     ;;show pixel position and value
-                    (send parent set-status-text (string-append "Value at pixel (" (number->string x)  ","  (number->string y)   ") is : "
-                                                              (image-value->string image x y)))
+                      (send parent set-status-text (string-append "Value at pixel (" (real->decimal-string x 2)  ","  (real->decimal-string y 2)   ") is : "
+                                                                  (image-value->string image x y)))
                     ;;errorcase
-                    (send parent set-status-text (string-append "Value at pixel ("  (number->string x)  "," (number->string y) ") is not defined!"))))))))
+                      (send parent set-status-text (string-append "Value at pixel ("  (real->decimal-string x 2)  "," (real->decimal-string y 2) ") is not defined!"))))))))))
     (super-new [style '(vscroll hscroll resize-corner)])))
   
 
