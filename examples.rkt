8b2ecdda1ff36c1f53c98854e53b0791b876b2f7
bseppke <benjamin.seppke@gmail.com>
2017-01-03 15:26:09 +0100

    Added example for image broadcasting and fixed splineimage view.

--- examples.rkt
+++ examples.rkt
@@ -84,7 +84,7 @@
 (matrix-set! t2mat 0 2 (/ (image-width img) 2.0))
 (matrix-set! t2mat 1 2 (/ (image-width img) 2.0))
 
-(define  img6 (image-map cut-8bit (affinewarpimage img (matrix-mult t1mat (matrix-mult rotmat t2mat)) 3)))
+(define  img5aff (image-map cut-8bit (affinewarpimage img (matrix-mult t1mat (matrix-mult rotmat t2mat)) 3)))
 
 (display "performing distance transform on canny edges of image")(newline)
  (define  img7 (distancetransform
@@ -102,9 +102,31 @@
 (define  img13 (medianfilter img 3 3))
 (define  img14 (nonlineardiffusion img 0.1 2.0))
 
+(display "testing splineimageview facilities")(newline)
+
+(define pos_x  34.23)
+(define pos_y 123.23)
+(define siv (create-splineimageview img 2))
+	(splineimageview-value siv pos_x pos_y)
+	(splineimageview-dx   siv pos_x pos_y)
+	(splineimageview-dy   siv pos_x pos_y)
+	(splineimageview-dxx  siv pos_x pos_y)
+	(splineimageview-dxy  siv pos_x pos_y)
+	(splineimageview-dyy  siv pos_x pos_y)
+	(splineimageview-dx3  siv pos_x pos_y)
+	(splineimageview-dxxy siv pos_x pos_y)
+	(splineimageview-dxyy siv pos_x pos_y)
+	(splineimageview-dy3  siv pos_x pos_y)
+	(splineimageview-g2   siv pos_x pos_y)
+	(splineimageview-g2x  siv pos_x pos_y)
+	(splineimageview-g2y  siv pos_x pos_y)
+	(splineimageview-g2xx siv pos_x pos_y)
+	(splineimageview-g2xy siv pos_x pos_y)
+	(splineimageview-g2yy siv pos_x pos_y)
+(void (delete-splineimageview siv))
 
 (show-image img5)
-(show-image img6 "title: img6")
+(show-image img5aff "title: img5aff")
 
 ;Test color images
 (define img1 (loadimage (build-path vigracket-path "images/lenna_face.png")))
@@ -114,6 +136,12 @@
 (show-image  (image->blue img1) "Lennas blue channel")
 (show-image  img1 "Lenna in RGB")
 (show-image  (paddimage img1 10 20 30 40) "Padded Lenna in RGB")
+
+(display "testing band broadcasting")(newline)
+(define img_t (image-map *
+                         img1 
+			(image-map (lambda (x) (if (< 100 x) 1.0 0.0))
+                                   (image->green img1))))
 
 ; Tensor tests
 (define img1_st  (structuretensor img1 1.0 4.0))
@@ -219,17 +247,18 @@
 (saveimage img3magnitude (build-path save-path "images/rect-fft-magnitude.png"))
 (saveimage (image-map sqrt img3magnitude) (build-path save-path "images/rect-fft-sqrt-magnitude.png"))
 
-(saveimage img4   (build-path save-path "images/blox-reflected-both.png"))
-(saveimage img5   (build-path save-path "images/blox-rotated-15deg.png"))
-(saveimage img6   (build-path save-path "images/blox-aff-rotated-15deg.png"))
-(saveimage img7   (build-path save-path "images/blox-disttransform-on-canny.png"))
-(saveimage img8   (build-path save-path "images/blox-diff_of_exp.png"))
-(saveimage img9   (build-path save-path "images/blox-gsmooth-3.0.png"))
-(saveimage img10  (build-path save-path "images/blox-log-3.0.png"))
-(saveimage img11  (build-path save-path "images/blox-gsharpening-0.5-3.0.png"))
-(saveimage img12  (build-path save-path "images/blox-sharpening-3.0.png"))
-(saveimage img13  (build-path save-path "images/blox-medianfilter-3x3.png"))
-(saveimage img14  (build-path save-path "images/blox-nonlineardiffusion-0.1-2.0.png"))
+(saveimage img4    (build-path save-path "images/blox-reflected-both.png"))
+(saveimage img5    (build-path save-path "images/blox-rotated-15deg.png"))
+(saveimage img5aff (build-path save-path "images/blox-aff-rotated-15deg.png"))
+(saveimage img_t   (build-path save-path  "images/lenna-masked.png"))
+(saveimage img7    (build-path save-path "images/blox-disttransform-on-canny.png"))
+(saveimage img8    (build-path save-path "images/blox-diff_of_exp.png"))
+(saveimage img9    (build-path save-path "images/blox-gsmooth-3.0.png"))
+(saveimage img10   (build-path save-path "images/blox-log-3.0.png"))
+(saveimage img11   (build-path save-path "images/blox-gsharpening-0.5-3.0.png"))
+(saveimage img12   (build-path save-path "images/blox-sharpening-3.0.png"))
+(saveimage img13   (build-path save-path "images/blox-medianfilter-3x3.png"))
+(saveimage img14   (build-path save-path "images/blox-nonlineardiffusion-0.1-2.0.png"))
 
 (saveimage shock_img1  (build-path save-path "images/lenna-shock-s2-r6-t03-i1.png"))
 (saveimage shock_img5  (build-path save-path "images/lenna-shock-s2-r6-t03-i5.png"))
